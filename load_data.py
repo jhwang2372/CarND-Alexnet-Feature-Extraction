bcdfd98c7d5ea703f6ebcc4481616d51648a43ec
jhwang2372 <jhwang2372@gmail.com>
2017-11-17 17:18:04 +0800

    change most of class labels to 1 except 1st class with label 0

--- load_data.py
+++ load_data.py
@@ -7,13 +7,19 @@ import csv
 nb_classes = 43
 epochs = 1
 batch_size = 128
-
+labels = []
 
 
 def createDictCSV(fileName="", dataDict={}):
     with open(fileName, "w") as csvFile:
         csvWriter = csv.writer(csvFile)
         for i,j in dataDict.items():
+            if i == 'labels':
+                labels = j
+                for index, value in enumerate(labels):
+                    if value:
+                        labels[index] = 1
+                j = labels
             csvWriter.writerow([i,j])
         csvFile.close()
 
@@ -22,4 +28,4 @@ def createDictCSV(fileName="", dataDict={}):
 with open('./train.p', 'rb') as f:
     data = pickle.load(f)
     createDictCSV("data_csv.csv", data)
-    print(len((data['labels'])))
+    print(data['labels'])
